091b50ff80e6d7ac66747b4787563529840f9ed8
sandesh23 <s.mahamure@gmail.com>
2022-06-02 10:36:02 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,21 @@
+
+from flask import Flask, render_template, request, flash,url_for
+
+app = Flask(__name__)
+app.secret_key = "bdsjkjkdsnVKJ"
+@app.route("/hello")
+def index():
+    flash("whats your name?")
+    return render_template ("index.html")
+
+@app.route("/greet/", methods =["POST","GET"])
+def greet():
+    flash("Hi " + str(request.form["name_input"]) + " great to see you! ")
+    return render_template("index.html")
+
+
+if __name__ == '__main__':
+    # db.create_all()
+    app.run(debug=True)
+
+
